78e908b7a6524c8c7187cafc63a3aed283d61b9d
dodo-nainggolan <valdo.henschel@gmail.com>
2019-11-04 18:21:18 +0700

    #2

--- app/src/main/java/com/example/mysettingpreference/MainActivity.java
+++ app/src/main/java/com/example/mysettingpreference/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.setting_holder, new MyPreferenceFragment()).commit();
+
     }
 }
